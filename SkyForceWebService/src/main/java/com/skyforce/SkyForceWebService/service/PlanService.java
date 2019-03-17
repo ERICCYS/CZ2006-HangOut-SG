53f123ecf0209254c95f3d401cb70d45303b51e4
WANGZIJIAN <jeremyw0001@gmail.com>
2019-03-17 20:16:51 +0800

    new classes added
    17Mar

---
+++ SkyForceWebService/src/main/java/com/skyforce/SkyForceWebService/service/PlanService.java
@@ -0,0 +1,16 @@
+package com.skyforce.SkyForceWebService.service;
+
+import com.skyforce.SkyForceWebService.model.Plan;
+
+import java.util.List;
+
+public interface PlanService {
+    List<Plan> findAll();
+
+    Plan findPlanById(Long id);
+
+    Plan save(Plan plan);
+
+    void delete(Plan plan);
+
+}
