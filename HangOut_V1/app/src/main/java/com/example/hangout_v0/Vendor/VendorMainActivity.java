af630717f443b882b3e4ee3c47407b87b90ce9e3
WANGZIJIAN <jeremyw0001@gmail.com>
2019-03-31 18:17:53 +0800

    Add vendor addshop function

--- HangOut_V1/app/src/main/java/com/example/hangout_v0/Vendor/VendorMainActivity.java
+++ HangOut_V1/app/src/main/java/com/example/hangout_v0/Vendor/VendorMainActivity.java
@@ -17,6 +17,8 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.hangout_v0.ApiCall.HangOutApi;
+import com.example.hangout_v0.ApiCall.HangOutData;
 import com.example.hangout_v0.Me.avator.EditCustomerProfile;
 import com.example.hangout_v0.R;
 import com.google.android.gms.tasks.Continuation;
@@ -29,25 +31,109 @@ import com.skyforce.SkyForceWebService.model.Shop;
 
 import com.example.hangout_v0.Vendor.Utils.ShopAdapterVendor;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class VendorMainActivity extends AppCompatActivity {
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
+public class VendorMainActivity extends AppCompatActivity {
+    public static final String baseUrl = "http://10.27.51.140:9090/api/";
     ListView listView;
     ImageView avator;
-    String url = "https://avatarfiles.alphacoders.com/873/thumb-87368.jpg";
+    //String url = "https://avatarfiles.alphacoders.com/873/thumb-87368.jpg";
     Button addShop, editAvatar;
     ShopAdapterVendor adapter;
     private String avatarUrl;
     private Uri avatarUri;
+    public Long id = new Long(1);
     private StorageReference storageRef;
     public static final int PICK_IMAGE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main_vendor);
+
+        //HangOutData.getVendor().getLong("id");
+//        getVendor(id);
+//        JSONObject vendorSelf = new JSONObject();
+//        vendorSelf = HangOutData.getVendor();
+
+        OkHttpClient client = new OkHttpClient();
+        String url = baseUrl + "vendor";
+        HttpUrl.Builder httpBuilder = HttpUrl.parse(url).newBuilder();
+        httpBuilder.addQueryParameter("vendorId", id.toString());
+        Request request = new Request.Builder().url(httpBuilder.build()).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String myResponse = response.body().string();
+                    try {
+                        JSONObject vendor = new JSONObject(myResponse);
+                        HangOutData.setVendor(vendor);
+                        // shops display listview
+                        listView = (ListView) findViewById(R.id.shopList);
+                        final ArrayList<Shop> arrayList = new ArrayList<>(); //you should change 'string' here to 'shop' later when connect with back end
+                        JSONArray shopList = new JSONArray();
+                        try {
+                            shopList = vendor.getJSONArray("shops");
+                            System.out.print("Get success");
+                            if (shopList != null) {
+                                for (int i=0;i<shopList.length();i++){
+                                    JSONObject shop = shopList.getJSONObject(i);
+                                    arrayList.add(new Shop(shop.getString("name"), shop.getString("contactNumber")));
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+//        arrayList.add(new Shop("PUTIEN Jurong", "12345678"));
+//        arrayList.add(new Shop("PUTIEN Changi", "91234567"));
+                        adapter = new ShopAdapterVendor(VendorMainActivity.this, arrayList);
+                        listView.setAdapter(adapter);
+                        //listview end
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id){
+
+                                String tempListView = arrayList.get(position).getName();
+                                Intent intent = new Intent(VendorMainActivity.this, com.example.hangout_v0.Vendor.VendorShop.class);
+                                intent.putExtra("ClickedValue", tempListView);
+                                startActivity(intent);
+
+                            }
+                        });
+
+
+
+
+                    } catch (JSONException e) {
+                        //textView.setText("Error");
+                    }
+                }
+            }
+        });
+
 
         addShop = (Button) findViewById(R.id.addShopButton);
         addShop.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +141,7 @@ public class VendorMainActivity extends AppCompatActivity {
             public void onClick(View v) {
 //                Toast.makeText(VendorMainActivity.this,"hi",Toast.LENGTH_SHORT).show();
                 Intent addyourshop = new Intent(VendorMainActivity.this, com.example.hangout_v0.Vendor.AddShop.class);
+                addyourshop.putExtra("vendorId", id);
                 startActivity(addyourshop);
             }
         });
@@ -76,26 +163,7 @@ public class VendorMainActivity extends AppCompatActivity {
         });
 
 
-        //listview
-        listView = (ListView) findViewById(R.id.shopList);
-        final ArrayList<Shop> arrayList = new ArrayList<>(); //you should change 'string' here to 'shop' later when connect with back end
 
-        arrayList.add(new Shop("PUTIEN Jurong", "12345678"));
-        arrayList.add(new Shop("PUTIEN Changi", "91234567"));
-        adapter = new ShopAdapterVendor(this, arrayList);
-        listView.setAdapter(adapter);
-        //listview end
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id){
-
-                String tempListView = arrayList.get(position).getName();
-                Intent intent = new Intent(VendorMainActivity.this, com.example.hangout_v0.Vendor.VendorShop.class);
-                intent.putExtra("ClickedValue", tempListView);
-                startActivity(intent);
-
-            }
-        });
 
     }
     public void populateListView(){
