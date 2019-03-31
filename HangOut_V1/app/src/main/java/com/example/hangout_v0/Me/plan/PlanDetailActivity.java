71baebacda1800db9002adcb424d54d5c0a18fb2
Eric Ma <eric.m.990909@gmail.com>
2019-03-31 22:05:27 +0800

    Get Plan Items from backend

--- HangOut_V1/app/src/main/java/com/example/hangout_v0/Me/plan/PlanDetailActivity.java
+++ HangOut_V1/app/src/main/java/com/example/hangout_v0/Me/plan/PlanDetailActivity.java
@@ -10,18 +10,29 @@ import android.widget.ListView;
 
 import com.example.hangout_v0.ApiCall.HangOutApi;
 import com.example.hangout_v0.Me.plan.PlanHistoryActivity;
+import com.example.hangout_v0.Me.reservation.ReservationActivity;
 import com.example.hangout_v0.R;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class PlanDetailActivity extends AppCompatActivity {
 
     ListView shopsInPlansListView;
-    ArrayList<String> shopName;
-    ArrayList<String> shopAddress;
-    ArrayList<String> shopDateTime;
+    private ArrayList<String> shopName = new ArrayList<>();
+    private ArrayList<String> shopAddress = new ArrayList<>();
+    private ArrayList<String> shopDateTime = new ArrayList<>();
     Button historyButton;
 
     @Override
@@ -36,14 +47,62 @@ public class PlanDetailActivity extends AppCompatActivity {
         Intent in= getIntent();
         String planName= in.getStringExtra("PlanName");
 
-        PlanDataStub planDataStub = new PlanDataStub("User1");
-
-        shopName= planDataStub.getPlanShopName(planName);
-        shopAddress = planDataStub.getPlanShopAddress(planName);
-        shopDateTime = planDataStub.getPlanShopDateTime(planName);
+//        PlanDataStub planDataStub = new PlanDataStub("User1");
 //
-        com.example.hangout_v0.Me.plan.ShopInPlanAdapter shopInPlanAdapter = new com.example.hangout_v0.Me.plan.ShopInPlanAdapter(this, shopName,shopAddress, shopDateTime);
-        shopsInPlansListView.setAdapter(shopInPlanAdapter);
+//        shopName= planDataStub.getPlanShopName(planName);
+//        shopAddress = planDataStub.getPlanShopAddress(planName);
+//        shopDateTime = planDataStub.getPlanShopDateTime(planName);
+
+
+        // My suggestion is that we should see the plans histories (a list of plans)
+        // Then, see the plans detail, (shops etc.)
+        // Because after we get all the plans, we know each plan id
+        // here we use a dummy planId
+        Long planId = 1l;
+
+
+        OkHttpClient client = new OkHttpClient();
+        String url = HangOutApi.baseUrl + "plan-items-formatted";
+        HttpUrl.Builder httpBuilder = HttpUrl.parse(url).newBuilder();
+        httpBuilder.addQueryParameter("planId", planId.toString());
+        Request request = new Request.Builder().url(httpBuilder.build()).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String myResponse = response.body().string();
+                    try {
+                        JSONArray plans = new JSONArray(myResponse);
+                        for (int i = 0; i < plans.length(); i++) {
+                            JSONObject plan = (JSONObject)plans.get(i);
+                            shopDateTime.add((String) plan.get("shopDateTime"));
+                            shopAddress.add((String)plan.get("shopName"));
+                            shopAddress.add((String)plan.get("shopAddress"));
+                        }
+                        PlanDetailActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                com.example.hangout_v0.Me.plan.ShopInPlanAdapter shopInPlanAdapter = new com.example.hangout_v0.Me.plan.ShopInPlanAdapter(PlanDetailActivity.this, shopName,shopAddress, shopDateTime);
+                                shopsInPlansListView.setAdapter(shopInPlanAdapter);
+                            }
+                        });
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+//
+//        com.example.hangout_v0.Me.plan.ShopInPlanAdapter shopInPlanAdapter = new com.example.hangout_v0.Me.plan.ShopInPlanAdapter(this, shopName,shopAddress, shopDateTime);
+//        shopsInPlansListView.setAdapter(shopInPlanAdapter);
 
 
        historyButton = findViewById(R.id.planHistoryButton);
