b65d9406ca45bfac55d701aeefbb5825c3527231
Yuhao <heyu0012@ntu.edu.sg>
2019-03-31 10:57:06 +0800

    add reservation list

---
+++ HangOut_V1/app/src/main/java/com/example/hangout_v0/Me/reservation/ReservationActivity.java
@@ -0,0 +1,51 @@
+package com.example.hangout_v0.Me.reservation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.hangout_v0.Me.plan.PlanDataStub;
+import com.example.hangout_v0.R;
+
+import java.util.ArrayList;
+
+public class ReservationActivity extends AppCompatActivity {
+
+    ListView reservationListView;
+
+    ArrayList<String> reservationShopName;
+    ArrayList<String> reservationShopAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.me_reservation_list);
+
+        this.getSupportActionBar().hide();
+
+        reservationListView = findViewById(R.id.me_reservation_reservationListView);
+
+        PlanDataStub planDataStub = new PlanDataStub("User1");
+//
+        reservationShopName= planDataStub.getAllShopName("Plan A"); // planName to replace hardcode "Plan A"
+        reservationShopAddress = planDataStub.getAllShopAddress("Plan A");
+//
+        com.example.hangout_v0.Me.reservation.ReservationAdaptor reservationAdaptor = new com.example.hangout_v0.Me.reservation.ReservationAdaptor(this, reservationShopName,reservationShopAddress);
+        reservationListView.setAdapter(reservationAdaptor);
+
+//        reservationListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view,
+//                                    int position, long id) {
+//                reservationShopName.remove(position);
+//                reservationShopAddress.remove(position);
+//
+//            }
+//        });
+
+
+    }
+
+}
---
+++ HangOut_V1/app/src/main/java/com/example/hangout_v0/Me/reservation/ReservationAdaptor.java
@@ -0,0 +1,68 @@
+package com.example.hangout_v0.Me.reservation;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.hangout_v0.R;
+
+import java.util.ArrayList;
+
+public class ReservationAdaptor extends BaseAdapter{
+
+    ArrayList<String> shopName;
+    ArrayList<String> shopAddress;
+    LayoutInflater mInflater;
+
+    public ReservationAdaptor(Context c, ArrayList<String> name, ArrayList<String> shopAdd){
+        shopAddress = shopAdd;
+        shopName = name;
+        mInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return shopName.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return shopName.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View v= mInflater.inflate(R.layout.me_reservation_shop_in_list,null);
+        TextView shopNameTextView =  v.findViewById(R.id.me_reservation_shopNameTextView);
+        TextView shopAddressTextView = v.findViewById(R.id.me_reservation_shopAddressTextView);
+
+        String name = shopName.get(position);
+        String address = shopAddress.get(position);
+
+        shopNameTextView.setText(name);
+        shopAddressTextView.setText(address);
+
+        //Handle buttons and add onClickListeners
+        ImageButton deleteBtn = (ImageButton) v.findViewById(R.id.me_reservation_shop_deleteBtn);
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                shopName.remove(position);
+                shopAddress.remove(position);
+            }
+        });
+
+        return v;
+    }
+}
+
+
