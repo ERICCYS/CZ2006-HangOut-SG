1bcb9c233a132583ab57457478e66ba11bf12dfc
Eric Ma <eric.m.990909@gmail.com>
2019-03-31 22:30:22 +0800

    Add API call for formatted data fetching

--- SkyForceWebService/src/main/java/com/skyforce/SkyForceWebService/controller/ReservationController.java
+++ SkyForceWebService/src/main/java/com/skyforce/SkyForceWebService/controller/ReservationController.java
@@ -3,15 +3,20 @@ package com.skyforce.SkyForceWebService.controller;
 import com.skyforce.SkyForceWebService.config.JSONConvert;
 import com.skyforce.SkyForceWebService.model.Customer;
 import com.skyforce.SkyForceWebService.model.Reservation;
+import com.skyforce.SkyForceWebService.model.Shop;
 import com.skyforce.SkyForceWebService.service.CustomerService;
 import com.skyforce.SkyForceWebService.service.ReservationService;
 import com.skyforce.SkyForceWebService.service.ShopService;
+import org.json.simple.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.json.simple.JSONObject;
+
 import javax.validation.Valid;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +36,28 @@ public class ReservationController {
     public String getAllReservations() {
         List<Reservation> reservations = reservationService.findAll();
         return JSONConvert.JSONConverter(reservations);
+    }
+
+    @GetMapping("reservation-customer-formatted")
+    public String getReservationsByCustomerIdFormatted(
+            @RequestParam Long customerId
+    ) {
+        List<Reservation> reservations = reservationService.findAll();
+        List<Reservation> interested = new ArrayList<>();
+        JSONArray relatedReservations = new JSONArray();
+        for (Reservation reservation : reservations) {
+            if (reservation.getCustomerId().equals(customerId)) {
+                interested.add(reservation);
+                Shop shop = shopService.findShopById(reservation.getShopId());
+                JSONObject newReservation = new JSONObject();
+                newReservation.put("shopName", shop.getName());
+                newReservation.put("shopAddress", shop.getLocation());
+                newReservation.put("arrivalTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(reservation.getArrivalTime()));
+                relatedReservations.add(newReservation);
+            }
+        }
+
+        return JSONConvert.JSONConverter(relatedReservations);
     }
 
     @GetMapping("/reservation")
