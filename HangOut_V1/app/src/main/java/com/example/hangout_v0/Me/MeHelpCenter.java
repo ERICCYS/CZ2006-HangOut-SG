0b41b796612f12b2ed8409419215ee616cfaac04
yinya998 <Yinya998@gmail.com>
2019-04-01 18:39:13 +0800

    add help center page

---
+++ HangOut_V1/app/src/main/java/com/example/hangout_v0/Me/MeHelpCenter.java
@@ -0,0 +1,16 @@
+package com.example.hangout_v0.Me;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+import com.example.hangout_v0.R;
+
+public class MeHelpCenter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.me_help_center);
+    }
+}
